7243d7d969e059726fe8d66b03a72f45e3affc3f
Angel Kirkov <angelnkirkov@gmail.com>
2021-11-03 15:27:15 +0200

    data edit

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,20 +1,20 @@
 export const WorkData = [
   {
     company: "Vivacom",
-    designation: "Software Engineer",
-    dateJoining: "02-2021",
+    designation: "Software Integration Engineer",
+    dateJoining: "02.2021",
     dateEnd: "Present",
     companyLogo:
       "https://www.vivacom.bg/web/files/resources/1/documents/Vivacom_rebranding_2021_logo_negative.png",
-    work: "Lorem ipsum",
+    work: "Lorem ipsum.",
   },
   {
-    company: "Company 2",
-    designation: "Software Engineer",
-    dateJoining: "02-02-2000",
-    dateEnd: "Present",
+    company: "SEGA Europe",
+    designation: "QA Software Testing Engineer",
+    dateJoining: "09.2020",
+    dateEnd: "12.2020",
     companyLogo:
-      "https://iconape.com/wp-content/png_logo_vector/facebook-f-logo-2019.png",
-    work: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "https://upload.wikimedia.org/wikipedia/commons/4/41/SEGA_logo.png",
+    work: "Lorem ipsum.",
   },
 ];
